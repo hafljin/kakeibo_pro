4d21f1d303965a597b372df1b0d9babb5bf0abc0
micheldesu <moneypro1024@gmail.com>
2025-07-08 20:11:41 +0900

    HomeのUI作成中...

---
+++ app/src/main/java/com/example/kakeibopro/AddFragment.kt
@@ -0,0 +1,4 @@
+package com.example.kakeibopro
+
+class AddFragment {
+}
---
+++ app/src/main/java/com/example/kakeibopro/HomeFragment.kt
@@ -0,0 +1,49 @@
+package com.example.kakeibopro
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val calender = Calendar.getInstance()
+        val dayFormat = SimpleDateFormat("E", Locale.getDefault())
+        val dateFormat = SimpleDateFormat("dd", Locale.getDefault())
+        val dateContainer = view.findViewById<LinearLayout>(R.id.dateContainer)
+
+        for(i in 0..20) {
+            val verticalLayout = LinearLayout(requireContext())
+            verticalLayout.orientation = LinearLayout.VERTICAL
+            verticalLayout.setPadding(16, 8, 16, 8)
+            val dayTextView = TextView(requireContext())
+            dayTextView.text = dayFormat.format(calender.time)
+            dayTextView.textSize = 20f
+            dayTextView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+
+            val dateTextView = TextView(requireContext())
+            dateTextView.text = dateFormat.format(calender.time)
+            dateTextView.textSize = 20f
+            dateTextView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+
+            verticalLayout.addView(dayTextView)
+            verticalLayout.addView(dateTextView)
+            dateContainer.addView(verticalLayout)
+
+            calender.add(Calendar.DAY_OF_MONTH, 1)
+        }
+        return view
+    }
+}
